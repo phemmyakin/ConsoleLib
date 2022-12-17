04515cc6cd5f1eb95646a3cb4fe2554c5f9eafa9
Akinnigbagbe Oluwafemi <oakinnigbagbe@eminenttechnology.com>
2022-12-17 17:45:43 +0000

    first message

---
+++ Book.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace LibraryManagement
+{
+    public class Book
+    {
+        public string title;
+        public string author;
+
+        public Book(string _title, string _author)
+        {
+            this.author = _author;
+            this.title = _title;
+        }
+
+        public Book()
+        {
+
+        }
+        public List<Book> FetchBooks(string response)
+        {
+            string[] splittedBooks;
+            Book book;
+            List<Book> books = new List<Book>();
+            string filePath = "../../asset/books.txt";
+            // ArrayList bookList = new ArrayList();
+            string[] allbooks = File.ReadAllLines(filePath);
+            foreach (string bookstring in allbooks)
+            {
+                splittedBooks = bookstring.Split(',');
+                book = new Book(splittedBooks[0], splittedBooks[1]);
+                books.Add(book);
+
+            }
+            return books;
+        }
+
+        public Dictionary<string, Book> GetAllBooks()
+        {
+            string[] splittedBooks;
+            Book book;
+            Dictionary<string, Book> dictionaryOfBooks = new Dictionary<string, Book>();
+            string filePath = "../../asset/books.txt";
+            string[] content = File.ReadAllLines(filePath);
+            foreach (string bookstring in content)
+            {
+                if (bookstring.Length > 0)
+                {
+                    splittedBooks = bookstring.Split(',');
+                    book = new Book(splittedBooks[0], splittedBooks[1]);
+
+                    //Only add books with title and authors to the list
+                    if (book.author.Length > 0 && book.title.Length > 0)
+                    {
+                        dictionaryOfBooks.Add(book.title, book);
+                    }
+                }
+            }
+            return dictionaryOfBooks;
+        }
+
+
+        public static string readString(string prompt)
+        {
+            string result;
+            do
+            {
+                Console.Write(prompt);
+                result = Console.ReadLine();
+            } while (result.Trim() == "");
+            return result;
+        }
+        public void BorrowBook(Dictionary<string, Book> books)
+        {
+            string samplefileUrl = "https://www.africau.edu/images/default/sample.pdf";
+            string booktitle = readString("\nPlease enter the title of the book you want to borrow: ");
+            //get a list of all keys and convert to lower case
+            List<string> bookKeys = new List<string>(books.Keys);
+            List<string> matchingRecords = new List<string>();
+            Dictionary<string, Book> matchedBook = new Dictionary<string, Book>();
+            for (int i = 0; i < bookKeys.Count; i++)
+            {
+                if (bookKeys[i].ToLower().Contains(booktitle.ToLower()))
+                {
+                    matchingRecords.Add(bookKeys[i]);
+                }
+            }
+            if (matchingRecords.Count > 0)
+            {
+                if (matchingRecords.Count > 1)
+                {
+                    Console.WriteLine("\nHere are the corresponding matches to your: ");
+                    for (int i = 0; i < matchingRecords.Count; i++)
+                    {
+                        Console.WriteLine((i + 1) + ". " + books[matchingRecords[i]].title + " by " + books[matchingRecords[i]].author);
+                        Book newBook = new Book
+                        {
+                            author = books[matchingRecords[i]].author,
+                            title = books[matchingRecords[i]].title
+                        };
+                        matchedBook.Add(books[matchingRecords[i]].title, newBook);
+                    }
+                    //make the user select one book at a time
+                    BorrowBook(matchedBook);
+
+                }
+                else
+                {
+                    Console.WriteLine("\nThis is the book");
+                    Console.WriteLine("\n" + (1) + ". " + books[matchingRecords[0]].title + " by " + books[matchingRecords[0]].author);
+                    Console.Write("\npress 1 to download or any key to view all available books again: ");
+                    string ress = Console.ReadLine();
+                    if (ress == "1")
+                    {
+                        string user = Program.globalUser;
+                        downloadFile(user,samplefileUrl, books[matchingRecords[0]].title);
+                        Console.WriteLine("\n" + books[matchingRecords[0]].title + " downloaded successfully.");
+                    }
+                    else
+                    {
+                        DisplayBooks();
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("There are no matches for your request");
+                DisplayBooks();
+
+            }
+        }
+
+
+        /*
+         code to download file from  gotten from :
+         https://stackoverflow.com/questions/307688/how-to-download-a-file-from-a-url-in-c
+        assessed 15/12/2022
+         */
+        private static void downloadFile(string user, string url, string file)
+        {           
+            string filename = file + ".pdf";
+            WebClient cln = new WebClient();
+            cln.DownloadFile(url, filename);
+            List<string> userData = new List<string>
+            {
+                user,
+                file
+            };
+            //userData.Add(user);
+            //userData.Add(file);
+            saveUserData(userData);
+        }
+
+        static void saveUserData(List<string> dataList)
+        {
+            string userName = dataList[0];
+            string fileName = dataList[1];           
+            string binFile = userName + ".bin";
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(binFile);
+            bf.Serialize(file, dataList);
+            file.Close();
+        }
+
+        public void DisplayBooks()
+        {
+            Dictionary<string, Book> allBooks;
+            allBooks = this.GetAllBooks();
+            if (allBooks.Count > 0)
+            {
+                Console.WriteLine("\nThese are the available books with authors, you can borrow one book at a time\n");
+                //order in ascending 
+                List<Book> bookList = new List<Book>(allBooks.Values).OrderBy(x => x.title).ToList();
+                for (int i = 0; i < bookList.Count; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + bookList[i].title + " by " + bookList[i].author);
+                }
+                BorrowBook(allBooks);
+            }
+            else
+            {
+                Console.WriteLine("\nThere are no books available right now");
+                DisplayBooks();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace LibraryManagement
+{
+    internal class Program
+    {
+        enum userOption
+        {
+            yes,
+            no
+        }
+        public static string globalUser;
+        static void Main(string[] args)
+        {
+            bool userExist = false;
+              string u = "";
+        string[] allUsers = getUsers();
+            string response;
+            Console.WriteLine("Welcome to HOE public library!!! ");
+            const string welcomeNote = "\nPlease enter your unique username:";
+            string userName = readString(welcomeNote);
+            globalUser = userName;
+            //Person person = new Person();  
+         
+            //person.savePerson(userName);
+
+            foreach (string user in allUsers)
+            {
+                if (userName == user)
+                {
+                    userExist = true;
+                }
+            }
+            //var cc = person.getPerson();
+            if (!userExist)
+            {
+                // ask for the user first name and last name
+                saveUser(userName);
+                //do all other stuff
+                //try
+                //{
+                //    DisplayResponse(response);
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex.Message);
+                //    //recovery code should be to start over again
+                //}
+            }
+            else
+            {
+                // recall the history of the user here and continue
+                //deserilaize the use record at this point
+                Console.WriteLine("\nWelcome back " + userName);
+                try
+                {
+                    List<string> fetchedData = getUserData(userName);
+                    string previousBookCollected = fetchedData[1];
+
+                    Console.WriteLine("\nYou previously collected " + previousBookCollected);
+                }
+                catch (Exception ex)
+                {
+
+                    //string binFile = userName + ".bin";
+                    
+                    //FileStream file = File.Create(binFile);
+                    //file.Close();
+                    //Console.WriteLine(ex.Message);
+                    Console.WriteLine("Sorry, I could not fetch your records, the file could be missing/corrupted");
+                 
+                }
+
+                //List<string>fetchedData = getUserData(userName);
+                //string previousBookCollected = fetchedData[1];
+
+                //Console.WriteLine("\nYou previously collected "+previousBookCollected);
+               
+            }
+
+            do
+            {
+                response = Intro("\nWould you like to borrow a book: ");
+                try
+                {
+                    DisplayResponse(response);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (response != userOption.no.ToString());
+
+
+          
+
+
+            Console.ReadLine();
+        }
+
+        public static string Intro(string message)
+        {
+            string result;
+            result = readString(message);
+            while (!validateResponse(result))
+            {
+                Console.WriteLine("Please enter 'Yes' or 'No'");
+                result = readString(message);
+            }
+            return result;
+        }
+        public static string readString(string prompt)
+        {
+            string result;
+            do
+            {
+                Console.Write(prompt);
+                result = Console.ReadLine();
+            } while (result.Trim() == "");
+            return result;
+        }
+
+        public static bool validateResponse(string answer)
+        {
+            if (answer.ToLower() == userOption.yes.ToString())
+            {
+                return true;
+            }
+            else if (answer.ToLower() == userOption.no.ToString())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public static void DisplayResponse(string response)
+        {
+            Book book = new Book();
+            //Dictionary<string, Book> allBooks;
+            if (response == userOption.yes.ToString())
+            {
+                book.DisplayBooks();
+            }
+            //else
+            //{
+            //    response = readString("Would you like to return a book: ");
+            //    while (!validateResponse(response))
+            //    {
+            //        Console.WriteLine("Please enter 'Yes' or 'No'");
+            //        response = readString("Would you like to return a book: ");
+            //    }
+            //    if (response == userOption.yes.ToString())
+            //    {
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("How can i help you?");
+            //    }
+            //}
+
+        }
+
+        static void saveUser(string userName)
+        {
+            string path = "../../asset/userList.txt";
+            var cc = File.OpenWrite(path);
+            cc.Close();
+            StreamWriter writer = new StreamWriter(path, true);
+            writer.WriteLine(userName);
+            writer.Close();
+        }
+        static string[] getUsers()
+        {
+            string path = "../../asset/userList.txt";
+            var cc = File.OpenWrite(path);
+            cc.Close();
+            var dd = File.ReadAllLines(path);
+            return dd;
+        }
+
+
+        //serilaize portion
+        //static void saveUserData(List<string> array)
+        //{
+        //    string binFile = array + ".bin";
+        //    BinaryFormatter bf = new BinaryFormatter();
+        //    FileStream file = File.Create(binFile);
+        //    bf.Serialize(file, array);
+        //    file.Close();
+        //}
+
+       // [Serializable]
+        static  List<string> getUserData( string user)
+        {
+            //handle missing file here
+            string fileName = user + ".bin";
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.OpenRead(fileName);
+            try
+            {            
+                List<string> array = (List<string>)bf.Deserialize(file);
+                file.Close();
+                return array;
+            }
+            catch (Exception)
+            {
+                file.Close();
+                throw;
+            }
+         
+        }
+    }
+
+    //public class Person
+    //{
+    //    public string userName { get; set; }
+       
+    //    public string getPerson()
+    //    {            
+    //        return this.userName;
+    //    }
+    //    public void savePerson(string _userName)
+    //    {
+    //        this.userName = _userName;
+            
+    //    }
+    //}
+}
